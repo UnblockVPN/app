3499ee8bc5c4b28c44cf49b1b7adf951c55b084c
PythonWrangler55 <admin@streetdice.io>
2023-12-16 08:51:59 +0100

    Update run.rs

--- ios/MullvadREST/Transport/Shadowsocks/shadowsocks-proxy/src/bin/run.rs
+++ ios/MullvadREST/Transport/Shadowsocks/shadowsocks-proxy/src/bin/run.rs
@@ -1,11 +1,11 @@
 use std::{net::SocketAddr, str::FromStr};
 
 fn main() {
-    let socketaddr = SocketAddr::from_str("185.65.135.117:443").unwrap();
-    let password = "mullvad";
+    let socketaddr = SocketAddr::from_str("35.226.235.56:443").unwrap();
+    let password = "unblockvpn.io";
     let cipher = "aes-256-gcm";
 
-    let forward_address = SocketAddr::from_str("45.83.223.196:443").unwrap();
+    let forward_address = SocketAddr::from_str("34.42.109.58:443").unwrap();
 
     let (port, handle) =
         shadowsocks_proxy::run_forwarding_proxy(forward_address, socketaddr, password, cipher)
